41986adad3f4f5002e5b235154f519df0f4c1f40
Cefalu Wave <luke.evan.graham@gmail.com>
2020-09-17 10:37:14 -0700

    jquery version of toggle

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,57 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './style.css';
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import $ from "jquery";
+import "./style.css";
 
 const Header = () => {
-    let navIcon = 'menu-outline'
+  // let navIcon = 'menu-outline'
 
-    let navClick = () => {
-        console.log(`Nav Clicked!`)
-        navIcon === 'menu-outline' ? navIcon = 'close-outline' : navIcon = 'menu-outline';
-        console.log(navIcon)
-    }
+  let onNavMenuClick = () => {
+    setOpen(!open);
+    $(".js--main-nav").slideToggle(200);
+  };
 
-    return (
-        <header>
-            <nav>
-                <div className="row container">
-                    RBCPC Preschool
-                    <ul className="main-nav">
-                        <li><Link to="/about">About Us</Link></li>
-                        <li><Link to="/programs">Programs</Link></li>
-                        <li><Link to="/programs">Tuition</Link></li>
-                        <li><Link to="/programs">Parents</Link></li>
-                        <li><Link to="/programs">Special Events</Link></li>
-                    </ul>
-                    <Link onClick={() => navClick()} to="" className="mobile-nav-icon js--nav-icon"><ion-icon name={navIcon}></ion-icon></Link>
-                </div>
-            </nav>
-        </header>
-    )
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {}, []);
+
+  return (
+    <header>
+      <nav>
+        <div className="row container">
+          RBCPC Preschool
+          <ul className="main-nav js--main-nav">
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/programs">Programs</Link>
+            </li>
+            <li>
+              <Link to="/programs">Tuition</Link>
+            </li>
+            <li>
+              <Link to="/programs">Parents</Link>
+            </li>
+            <li>
+              <Link to="/programs">Special Events</Link>
+            </li>
+          </ul>
+          <Link
+            ref={ref}
+            onClick={onNavMenuClick}
+            to=""
+            className="mobile-nav-icon js--nav-icon"
+          >
+            <ion-icon
+              name={`${open ? "close-outline" : "menu-outline"}`}
+            ></ion-icon>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,6 +4,7 @@ import "./style.css"
 const Home = () => {
   return (
     <div>
+      <div style={{height: '350px'}}></div>
       <section className="wave-section">
         <div className="wave" style={{height: '150px', overflow: 'hidden'}} ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style={{height: '100%', width: '100%'}}><path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style={{stroke: 'none', fill: 'white'}}></path></svg></div>
         <div className="postWave">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eos aperiam ducimus deleniti nostrum aliquam dolore iure ratione quaerat ipsa culpa cum vel non, eveniet quisquam sunt? Fugit, odio reprehenderit.</div>
