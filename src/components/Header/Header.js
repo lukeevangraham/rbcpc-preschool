ba3943a764c0512d90755386001e9717662ebdfd
Cefalu Wave <luke.evan.graham@gmail.com>
2020-09-16 23:00:44 -0700

    working on nav button functionality

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,14 @@ import { Link } from 'react-router-dom';
 import './style.css';
 
 const Header = () => {
+    let navIcon = 'menu-outline'
+
+    let navClick = () => {
+        console.log(`Nav Clicked!`)
+        navIcon === 'menu-outline' ? navIcon = 'close-outline' : navIcon = 'menu-outline';
+        console.log(navIcon)
+    }
+
     return (
         <header>
             <nav>
@@ -11,7 +19,11 @@ const Header = () => {
                     <ul className="main-nav">
                         <li><Link to="/about">About Us</Link></li>
                         <li><Link to="/programs">Programs</Link></li>
+                        <li><Link to="/programs">Tuition</Link></li>
+                        <li><Link to="/programs">Parents</Link></li>
+                        <li><Link to="/programs">Special Events</Link></li>
                     </ul>
+                    <Link onClick={() => navClick()} to="" className="mobile-nav-icon js--nav-icon"><ion-icon name={navIcon}></ion-icon></Link>
                 </div>
             </nav>
         </header>
